5ac53d78feb4503bd0db5ea578120ab5f3eba40b
Julia Torres <99496598+JuhCarol@users.noreply.github.com>
2022-02-25 19:55:12 -0300

    Add files via upload

---
+++ Ingressos de show.py
@@ -0,0 +1,7 @@
+import math
+custoShow = float (input('Preço do show: '))
+ingresso = float (input('Preço do ingresso: '))
+cobrir = custoShow / ingresso
+lucro = 1.23 * custoShow / ingresso
+print ('Quantos convites são necessários para cobrir o custo?:', math.ceil(cobrir))
+print ('Quantos convites são necessários para lucrar 23%?:', math.ceil(lucro))
